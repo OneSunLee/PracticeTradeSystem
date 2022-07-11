8b3d11962a9f843501a7f5a437c7909047354bd3
Miloomac <kuaile.1314@live.cn>
2022-07-12 01:58:09 +0800

    Finish the whole project!!!

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 原版的实现是个本科生，用c，每个函数都打开文件，统计行数，存入数组，存数组的开销很大;
 函数没有返回，是靠递归调用showmenu实现到达初始界面，最终内存占用越来越大，改成主函数中的循环;
-原版是静态数组，改成了vector<>。
+原版是静态数组，不合理，我偷懒改成了vector<>，如果自己写数据结构写List应该更符合出题人意图。
 
 原版c没有用传址，我c++传引用，没太大差别。
 
@@ -18,15 +18,17 @@
 #include <string.h>
 #include <fstream>
 #include <vector>
+#include <algorithm>
+#include <iterator>
 //商品信息
 struct glocery_item
 {
-	char number[128];	  //商品编号
-	char name[128];		  //商品名称
-	double buying_price;  //商品进价
-	double selling_price; //商品售价
-	char origin[128];	  //商品产地
-	int inventory;		  //商品库存;
+	char number[128];	   //商品编号
+	char name[128];		   //商品名称
+	double buying_price;   //商品进价
+	double selling_price;  //商品售价
+	char origin[128] = ""; //商品产地
+	int inventory;		   //商品库存;
 };
 
 //所需功能函数id
@@ -39,6 +41,11 @@ void remove_item(std::vector<glocery_item> &shop);	   //删除商品
 void modify_item(std::vector<glocery_item> &shop);	   //修改商品
 void search_item(std::vector<glocery_item> &shop);	   //商品查询
 void sort_items(std::vector<glocery_item> &shop);	   //商品库存排序
+template <int i>
+bool glocery_cmp_operator(const glocery_item &a, const glocery_item &b); //排序用到的比较函数
+//用模板switch不算是面向对象的方法，想通过传三个参数实现功能，也没办法了，在维护的时候一般会麻烦点，但是本程序比较简单，不会有这样的问题。
+//当然用函数式编程实现类似内嵌函数的语法更易读，所以这里只是个练习。查询那里用的是函数式语法。
+
 /*void bctc(int);//保存信息并退出系统 */
 //主函数
 int main()
@@ -139,7 +146,6 @@ void showmenu(std::vector<glocery_item> &shop)
 		break;
 	case 6: //商品库存排序
 		sort_items(shop);
-		save_file(shop);
 		break;
 	default:
 		std::cout << "输入错误，请重试！" << std::endl;
@@ -151,13 +157,12 @@ void showmenu(std::vector<glocery_item> &shop)
 //--------------------------显示商品--------------------------
 void show_items(std::vector<glocery_item> &shop)
 {
-	int i;
-	std::cout << "\t\t\t"
-			  << "编号" << '\t' << "名称" << '\t' << "买价" << '\t' << "卖价" << '\t' << "产地" << '\t' << "库存" << std::endl;
-	for (i = 0; i < shop.size(); i++)
+	std::cout << "编号" << '\t' << "名称" << '\t' << "进价" << '\t' << "售价" << '\t' << "产地" << '\t' << "库存" << std::endl;
+	for (const auto &temp : shop)
 	{
-		std::cout << "第" << i + 1 << "个商品信息为：\t";
-		std::cout << shop[i].number << '\t' << shop[i].name << '\t' << shop[i].buying_price << '\t' << shop[i].selling_price << '\t' << shop[i].origin << '\t' << shop[i].inventory << std::endl;
+		std::cout << temp.number << '\t' << temp.name << '\t'
+				  << temp.buying_price << '\t' << temp.selling_price << '\t'
+				  << temp.origin << '\t' << temp.inventory << std::endl;
 	}
 	std::cout << "\n商品信息显示完毕！按任意键返回。" << std::endl;
 	std::cin.ignore();
@@ -238,7 +243,7 @@ void remove_item(std::vector<glocery_item> &shop)
 		std::cin.ignore();
 		std::cin.get();
 	}
-	else if(iter==shop.end())
+	else if (iter == shop.end())
 	{
 		std::cout << "已遍历全部商品。按任意键退出。" << std::endl;
 		std::cin.ignore();
@@ -250,8 +255,8 @@ void modify_item(std::vector<glocery_item> &shop)
 {
 
 	std::string cmp;
-	bool find_flag = false;//是否找到
-	int choose = 0;//选择第几项
+	bool find_flag = false; //是否找到
+	int choose = 0;			//选择第几项
 	std::vector<glocery_item>::iterator iter;
 	std::cout << "请输入要修改的商品的编号或名称：" << std::endl;
 	std::cin >> cmp;
@@ -285,8 +290,8 @@ void modify_item(std::vector<glocery_item> &shop)
 					std::cin >> choose;
 					system("clear");
 					std::cout << (*iter).number << '\t' << (*iter).name
-						  << '\t' << (*iter).buying_price << '\t' << (*iter).selling_price
-						  << '\t' << (*iter).origin << '\t' << (*iter).inventory << std::endl;
+							  << '\t' << (*iter).buying_price << '\t' << (*iter).selling_price
+							  << '\t' << (*iter).origin << '\t' << (*iter).inventory << std::endl;
 					switch (choose)
 					{
 					case 1:
@@ -321,7 +326,7 @@ void modify_item(std::vector<glocery_item> &shop)
 					}
 				} while (choose != 0);
 			}
-			std::cout << "是否继续查找\""<<cmp<<"\"？y继续；0退出。" << std::endl;
+			std::cout << "是否继续查找\"" << cmp << "\"？y继续；0退出。" << std::endl;
 			std::cin.ignore();
 			if (std::cin.get() != 'y')
 				break;
@@ -333,7 +338,7 @@ void modify_item(std::vector<glocery_item> &shop)
 		std::cin.ignore();
 		std::cin.get();
 	}
-	else if(iter==shop.end())
+	else if (iter == shop.end())
 	{
 		std::cout << "已遍历全部商品。按任意键退出。" << std::endl;
 		std::cin.ignore();
@@ -343,289 +348,202 @@ void modify_item(std::vector<glocery_item> &shop)
 //--------------------------商品查询-----------------------------
 void search_item(std::vector<glocery_item> &shop)
 {
-	char a[128];
-	FILE *fp;
-	fp = fopen("items.csv", "r");
-	if (fp == NULL)
-	{
-		std::cout << "打开文件失败，请重试！" << std::endl;
-		std::cin.get();
-		fclose(fp);
-		exit(0);
-	}
-	int c = 0, i, flag = 0;
-	//读取商品信息；
-	while (!feof(fp))
-	{
-		fscanf(fp, "%s%s%lf%lf%s%d", shop[c].number, shop[c].name, &shop[c].buying_price, &shop[c].selling_price, shop[c].origin, &shop[c].inventory);
-		c++;
-	}
-	fclose(fp);
+	auto result = shop.end();
 	int choose;
 	do
 	{
-		std::cout << "★★★★★★★★★★★★★★★★★★★★★★★★★★★★" << std::endl;;
+		std::cout << "★★★★★★★★★★★★★★★★★★★★★★★★★★★★" << std::endl;
+		;
 		std::cout << "★请输入查询方式:★" << std::endl;
 		std::cout << "★☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆★" << std::endl;
 		std::cout << "★1.按商品编号查询商品信息！★" << std::endl;
 		std::cout << "★2.按商品名称查询商品信息！★" << std::endl;
-		std::cout << "★3.按商品产地查询商品信息！★" << std::endl;
-		std::cout << "★0.按0退出查询系统！★" << std::endl;
+		std::cout << "★3.按商品进价查询商品信息！★" << std::endl;
+		std::cout << "★4.按商品售价查询商品信息！★" << std::endl;
+		std::cout << "★5.按商品产地查询商品信息！★" << std::endl;
+		std::cout << "★6.按商品库存查询商品信息！★" << std::endl;
+		std::cout << "★0.按0退出查询系统！     ★" << std::endl;
 		std::cout << "★★★★★★★★★★★★★★★★★★★★★★★★★★★★" << std::endl;
-		std::cout << "请选择0~3" << std::endl;
-		scanf("%d", &choose);
+		std::cout << "请选择0~6" << std::endl;
+		std::cin >> choose; //选择操作
 		switch (choose)
 		{
-		case 1:
-			std::cin.get();
-			std::cout << "请输入商品编号：" << std::endl;
-			scanf("%s", a);
-			
-			for (i = 0; i < c; i++)
-			{
-				if (strcmp(a, shop[i].number) == 0)
-				{
-					flag = 1;
-					break;
-				}
-				else
-				{
-					flag = 0;
-				}
-			}
-			if (flag == 1)
-			{
-				std::cout << "该商品的信息如下：" << std::endl;
-				std::cout << "商品编号\t商品名称\t商品进价\t商品售价\t商品产地\t商品库存\t" << std::endl;
-				std::cout << shop[i].number << "\t\t" << shop[i].name << "\t\t" << shop[i].buying_price << "\t\t" << shop[i].selling_price << "\t\t" << shop[i].origin << "\t\t" << shop[i].inventory << "\t\t" << std::endl;
-			}
-			else if (flag == 0)
-			{
-				std::cout << "查无此商品！" << std::endl;
-				std::cin.get();
-			}
-			break;
-		case 2:
-			std::cin.get();
-			std::cout << "请输入商品名称：" << std::endl;
-			scanf("%s", a);
-			for (i = 0; i < c; i++)
-			{
-				if (strcmp(a, shop[i].name) == 0)
-				{
-					flag = 1;
-					break;
-				}
-				else
-				{
-					flag = 0;
-				}
-			}
-			if (flag == 1)
-			{
-				std::cout << "该商品的信息如下：" << std::endl;
-				std::cout << "商品编号\t商品名称\t商品进价\t商品售价\t商品产地\t商品库存\t" << std::endl;
-				std::cout << shop[i].number << "\t\t" << shop[i].name << "\t\t" << shop[i].buying_price << "\t\t" << shop[i].selling_price << "\t\t" << shop[i].origin << "\t\t" << shop[i].inventory << "\t\t" << std::endl;
-			}
-			else if (flag == 0)
-			{
-				std::cout << "查无此商品！" << std::endl;
-				std::cin.get();
-			}
-			break;
-		case 3:
-			std::cin.get();
-			std::cout << "请输入商品产地：" << std::endl;
-			scanf("%s", a);
-			for (i = 0; i < c; i++)
-			{
-				if (strcmp(a, shop[i].origin) == 0)
-				{
-					flag = 1;
-					break;
-				}
-				else
-				{
-					flag = 0;
-				}
-			}
-			if (flag == 1)
-			{
-				std::cout << "该商品的信息如下：" << std::endl;
-				std::cout << "商品编号\t商品名称\t商品进价\t商品售价\t商品产地\t商品库存\t" << std::endl;
-				std::cout << shop[i].number << "\t\t" << shop[i].name << "\t\t" << shop[i].buying_price << "\t\t" << shop[i].selling_price << "\t\t" << shop[i].origin << "\t\t" << shop[i].inventory << "\t\t" << std::endl;
-			}
-			else if (flag == 0)
-			{
-				std::cout << "查无此商品！" << std::endl;
-				std::cin.get();
-			}
-			break;
 		case 0:
 			std::cout << "成功退出商品查询系统！" << std::endl;
 			std::cin.get();
 			break;
+		case 1:
+		{
+			decltype(glocery_item::number) temp;
+			std::cout << "请输入商品编号：" << std::endl;
+			std::cin >> temp;
+			result = std::find_if(shop.begin(), shop.end(), [temp](const glocery_item &a)
+								  { return strcmp(a.number, temp) == 0; });
+		} //给temp加作用域；
+		break;
+
+		case 2:
+		{
+			decltype(glocery_item::name) temp;
+			std::cout << "请输入商品名称：" << std::endl;
+			std::cin >> temp;
+			result = std::find_if(shop.begin(), shop.end(), [temp](const glocery_item &a)
+								  { return strcmp(a.name, temp) == 0; });
+		}
+		break;
+		case 3:
+		{
+			decltype(glocery_item::buying_price) temp;
+			std::cout << "请输入商品进价：" << std::endl;
+			std::cin >> temp;
+			result = std::find_if(shop.begin(), shop.end(), [temp](const glocery_item &a)
+								  { return a.buying_price == temp; });
+		}
+		break;
+		case 4:
+		{
+			decltype(glocery_item::selling_price) temp;
+			std::cout << "请输入商品售价：" << std::endl;
+			std::cin >> temp;
+			result = std::find_if(shop.begin(), shop.end(), [temp](const glocery_item &a)
+								  { return a.selling_price == temp; });
+		}
+		break;
+		case 5:
+		{
+			decltype(glocery_item::origin) temp;
+			std::cout << "请输入商品产地：" << std::endl;
+			std::cin >> temp;
+			result = std::find_if(shop.begin(), shop.end(), [temp](const glocery_item &a)
+								  { return strcmp(a.origin, temp) == 0; });
+		}
+		case 6:
+		{
+			decltype(glocery_item::inventory) temp;
+			std::cout << "请输入商品库存：" << std::endl;
+			std::cin >> temp;
+			result = std::find_if(shop.begin(), shop.end(), [temp](const glocery_item &a)
+								  { return a.inventory == temp; });
+		}
+		break;
+			break;
+
 		default:
 			std::cout << "输入错误选项，请按任意键重新输入！" << std::endl;
 			std::cin.get();
 			system("clear");
 			break;
 		}
+		if (result != shop.end())
+		{
+			std::cout << "该商品的信息如下：" << std::endl;
+			std::cout << "商品编号\t商品名称\t商品进价\t商品售价\t商品产地\t商品库存\t" << std::endl;
+			std::cout << (*result).number << "\t\t" << (*result).name << "\t\t"
+					  << (*result).buying_price << "\t\t" << (*result).selling_price << "\t\t"
+					  << (*result).origin << "\t\t" << (*result).inventory << "\t\t" << std::endl;
+			std::cin.get();
+		}
+		else if (choose != 0)
+		{
+			std::cout << "查无此商品！" << std::endl;
+			std::cin.get();
+		}
 		if (choose != 0)
 		{
+			result = shop.end();
 			std::cout << "按任意键继续进行查询！" << std::endl;
 			std::cin.get();
 			system("clear");
 		}
 	} while (choose != 0);
 }
-
-	bool compare_opreator(const glocery_item a, const glocery_item b, std::string c){
-		if(c=="")
-		{
-			;
-		}
-		else
-		{
-			;
-			;
-		}
-		return a.name<b.name;
-	};
+//--------------------------排序用比较函数-----------------------------
+template <int i>
+bool glocery_cmp_operator(const glocery_item &a, const glocery_item &b)
+{
+	switch (i)
+	{
+	case 1:
+		return strcmp(a.number, b.number) < 0;
+	case 2:
+		return strcmp(a.name, b.name) < 0;
+	case 3:
+		return a.buying_price < b.buying_price;
+	case 4:
+		return a.selling_price < b.selling_price;
+	case 5:
+		return strcmp(a.origin, b.origin) < 0;
+		//return a.origin < b.origin;
+	case 6:
+		return a.inventory < b.inventory;
+	}
+}
 //--------------------------商品排序函数--------------------------
 void sort_items(std::vector<glocery_item> &shop)
 {
-
-	FILE *fp;
-	glocery_item t;
-	int c = 0, min = 0;
-	fp = fopen("items.csv", "r");
-	if (fp == NULL)
-	{
-		std::cout << "打开文件失败，请重试！" << std::endl;
-		std::cin.get();
-		fclose(fp);
-		exit(0);
-	}
-	while (!feof(fp))
-	{
-		fscanf(fp, "%s%s%lf%lf%s%d", shop[c].number, shop[c].name,
-		&shop[c].buying_price, &shop[c].selling_price,
-		shop[c].origin, &shop[c].inventory);
-		c++;
-	}
-	fclose(fp);
-	int choose, i, j;
+	int choose;
 	do
 	{
 		std::cout << "请输入排序方式!" << std::endl;
-		std::cout << "★★★★★★★★★★★★★★★★★★★" << std::endl;
-		std::cout << "★1.按商品的进价排序！★" << std::endl;
-		std::cout << "★☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆★" << std::endl;
-		std::cout << "★2.按商品的售价排序！★" << std::endl;
-		std::cout << "★☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆★" << std::endl;
-		std::cout << "★3.按商品库存量排序！★" << std::endl;
-		std::cout << "★☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆★" << std::endl;
-		std::cout << "★0.按0-退出排序系统！★" << std::endl;
-		std::cout << "★★★★★★★★★★★★★★★★★★★" << std::endl;
-		std::cout << "请选择0~3" << std::endl;
-		scanf("%d", &choose);
+		std::cout << "★★★★★★★★★★★★★★★★★★★★★★★★" << std::endl;
+		std::cout << "★1.按商品的编号排序！  ★" << std::endl;
+		std::cout << "★☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆★" << std::endl;
+		std::cout << "★2.按商品的名称排序！  ★" << std::endl;
+		std::cout << "★☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆★" << std::endl;
+		std::cout << "★3.按商品的进价排序！  ★" << std::endl;
+		std::cout << "★☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆★" << std::endl;
+		std::cout << "★4.按商品的售价排序！  ★" << std::endl;
+		std::cout << "★☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆★" << std::endl;
+		std::cout << "★5.按商品产地排序！    ★" << std::endl;
+		std::cout << "★☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆★" << std::endl;
+		std::cout << "★6.按商品库存量排序！  ★" << std::endl;
+		std::cout << "★☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆★" << std::endl;
+		std::cout << "★0.退出排序系统！      ★" << std::endl;
+		std::cout << "★★★★★★★★★★★★★★★★★★★★★★★★" << std::endl;
+		std::cout << "请选择0~6" << std::endl;
+		std::cin >> choose; //选择操作
 		switch (choose)
 		{
-		case 1: //运用了选择排序思想
-			for (i = 0; i < c - 1; i++)
-			{ // c=6
-				min = i; //最进价最高的商品;
-				for (j = i + 1; j < c; j++)
-				{ // c=7;
-					if (shop[min].buying_price > shop[j].buying_price)
-					{ //如果较小，排名下降一名；
-						min = j;
-					}
-				}
-				t = shop[i];
-				shop[i] = shop[min];
-				shop[min] = t;
-			}
-			fp = fopen("items.csv", "w");
-			if (fp == NULL)
-			{
-				std::cout << "打开文件失败，请重试！" << std::endl;
-				std::cin.get();
-				fclose(fp);
-				exit(0);
-			}
-			for (i = 0; i < c; i++)
-			{
-				fprintf(fp, "%s\t%s\t%.2lf\t%.2lf\t%s\t%d\n", shop[i].number, shop[i].name, shop[i].buying_price, shop[i].selling_price, shop[i].origin, shop[i].inventory);
-			}
-			fclose(fp);
-			std::cout << "按进价排序完成！按任意键返回！" << std::endl;
-			std::cin.get();
+		case 1: // sort默认内省排序，即快速排序转堆排序，复杂度O(nlogn）
+			std::sort(shop.begin(), shop.end(), glocery_cmp_operator<1>);
+			save_file(shop);
+			std::cout << "按编号排序完成！" << std::endl;
+			show_items(shop);
 			system("clear");
 			break;
 		case 2:
-			for (i = 0; i < c - 1; i++)
-			{			 // c=6
-				min = i; //最售价最高的商品;
-				for (j = i + 1; j < c; j++)
-				{ // c=7;
-					if (shop[min].selling_price > shop[j].selling_price)
-					{ //如果较小，排名下降一名；
-						min = j;
-					}
-				}
-				t = shop[i];
-				shop[i] = shop[min];
-				shop[min] = t;
-			}
-			fp = fopen("items.csv", "w");
-			if (fp == NULL)
-			{
-				std::cout << "打开文件失败，请重试！" << std::endl;
-				std::cin.get();
-				fclose(fp);
-				exit(0);
-			}
-			for (i = 0; i < c; i++)
-			{
-				fprintf(fp, "%s\t%s\t%.2lf\t%.2lf\t%s\t%d\n", shop[i].number, shop[i].name, shop[i].buying_price, shop[i].selling_price, shop[i].origin, shop[i].inventory);
-			}
-			fclose(fp);
-			std::cout << "按售价排序完成！按任意键返回！" << std::endl;
-			std::cin.get();
+			std::sort(shop.begin(), shop.end(), glocery_cmp_operator<2>);
+			save_file(shop);
+			std::cout << "按名称排序完成！" << std::endl;
+			show_items(shop);
 			system("clear");
 			break;
 		case 3:
-			for (i = 0; i < c - 1; i++)
-			{			 // c=6
-				min = i; //库存量最高的商品;
-				for (j = i + 1; j < c; j++)
-				{ // c=7;
-					if (shop[min].inventory > shop[j].inventory)
-					{ //如果较小，排名下降一名；
-						min = j;
-					}
-				}
-				t = shop[i];
-				shop[i] = shop[min];
-				shop[min] = t;
-			}
-			fp = fopen("items.csv", "w");
-			if (fp == NULL)
-			{
-				std::cout << "打开文件失败，请重试！" << std::endl;
-				std::cin.get();
-				fclose(fp);
-				exit(0);
-			}
-			for (i = 0; i < c; i++)
-			{
-				fprintf(fp, "%s\t%s\t%.2lf\t%.2lf\t%s\t%d\n", shop[i].number, shop[i].name, shop[i].buying_price, shop[i].selling_price, shop[i].origin, shop[i].inventory);
-			}
-			fclose(fp);
-			std::cout << "按库存量排序完成！按任意键返回！" << std::endl;
-			std::cin.get();
+			std::sort(shop.begin(), shop.end(), glocery_cmp_operator<3>);
+			save_file(shop);
+			std::cout << "按进价排序完成！" << std::endl;
+			show_items(shop);
+			system("clear");
+			break;
+		case 4:
+			std::sort(shop.begin(), shop.end(), glocery_cmp_operator<4>);
+			save_file(shop);
+			std::cout << "按售价排序完成！" << std::endl;
+			show_items(shop);
+			system("clear");
+			break;
+		case 5:
+			std::sort(shop.begin(), shop.end(), glocery_cmp_operator<5>);
+			save_file(shop);
+			std::cout << "按原产地排序完成！" << std::endl;
+			show_items(shop);
+			system("clear");
+			break;
+		case 6:
+			std::sort(shop.begin(), shop.end(), glocery_cmp_operator<6>);
+			save_file(shop);
+			std::cout << "按库存量排序完成！" << std::endl;
+			show_items(shop);
 			system("clear");
 			break;
 		case 0:
